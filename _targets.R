041190273cb86803089823eea891180980fc9ffb
Brian O'Meara <omeara.brian@gmail.com>
2024-07-16 10:33:54 -0400

    auto-update

--- _targets.R
+++ _targets.R
@@ -7,7 +7,7 @@ source("functions.R")
 list(
 	tar_target(data_2023, get_data('2023')),
 	tar_target(data_2024, get_data('2024')),
-	tar_target(covid_data, dplyr::bind_rows(data_2023, data_2024)),
+	tar_target(covid_data, dplyr::bind_rows(data_2023, data_2024)),	
 	tar_target(covid_by_time, aggregate_by_state_and_week(covid_data)),
 	tar_target(covid_by_time_US_total, aggregate_by_week(covid_data)),
 	tar_target(covid_cleaned, clean_data(covid_data)),
