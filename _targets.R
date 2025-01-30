3880705fa83e136010acabc105b4d2977df30509
Brian O'Meara <omeara.brian@gmail.com>
2025-01-30 09:13:50 -0500

    auto-update

--- _targets.R
+++ _targets.R
@@ -12,6 +12,7 @@ list(
 	tar_target(data_2024, get_data('2024')),
 	tar_target(data_2025, get_data('2025')),
 	tar_target(covid_data, dplyr::bind_rows(data_2020, data_2021, data_2022, data_2023, data_2024, data_2025)),	
+	tar_target(save_all_data, save(covid_data, file = "covid_data.rda")),
 	tar_target(covid_by_time, aggregate_by_state_and_week(covid_data)),
 	tar_target(covid_by_time_US_total, aggregate_by_week(covid_data)),
 	tar_target(covid_cleaned, clean_data(covid_data)),
